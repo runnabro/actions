15dd85b4386f4c5aab826a38efd20998cc240e32
podviaznikov <anton@podviaznikov.com>
2019-02-28 14:39:05 -0800

    wip

--- exchange-sync/entrypoint.sh
+++ exchange-sync/entrypoint.sh
@@ -45,23 +45,28 @@ echo ${ORG_ID}
 echo ${ASSET_ID}
 echo ${RAML_PATH}
 echo ${MAIN_FILE}
-## TODO if master and PUSH we need to do `latest-tag-next-SNAPSHOT`
+
+LAST_TAG=$(git describe --abbrev=0 --tags || echo "0.0.1")
+
+echo "Git latest tags: ${LAST_TAG}"
+
+## if push the version is latest tag + '-NEXT'
 if [[ $GITHUB_EVENT_NAME == "push" ]]; then
-  echo "handle release"
-  TAG="$(jq -r ".release.tag_name" "$GITHUB_EVENT_PATH")"
-  ASSET_VERSION=TAG
-  # TODO check that action is published
+  echo "handle push"
+  ASSET_VERSION="${LAST_TAG}-NEXT"
 fi
+## if release ASSET version is from the release
 if [[ $GITHUB_EVENT_NAME == "release" ]]; then
   echo "handle release"
   TAG="$(jq -r ".release.tag_name" "$GITHUB_EVENT_PATH")"
   ASSET_VERSION=TAG
   # TODO check that action is published
 fi
+## if pull request the version is latest tag + '-PR-{NUMBER}'
 if [[ $GITHUB_EVENT_NAME == "pull_request" ]]; then
   echo "handle pull_request"
-  PR_NUMBER="$(jq -r ".number" "$GITHUB_EVENT_PATH")"
-  ASSET_VERSION="pr-${PR-NUMBER}-SNAPSHOT"
+  PR_NUMBER=1 #"$(jq -r ".number" "$GITHUB_EVENT_PATH")"
+  ASSET_VERSION="${LAST_TAG}-PR-${PR_NUMBER}"
   # TODO check that action is sync or open
 fi
 
@@ -79,7 +84,7 @@ status_code=$(curl -v -i -X POST \
    -F "classifier=raml" \
    -F "groupId=${ORG_ID}" \
    -F "assetId=${ASSET_ID}" \
-   -F "version=ASSET_VERSION" \
+   -F "version=${ASSET_VERSION}" \
    -F "main=${MAIN_FILE}" \
    -F "organizationId=${ORG_ID}" \
    -F "someFileName=@\"raml.zip\";type=application/zip;filename=\"raml.zip\"" \
