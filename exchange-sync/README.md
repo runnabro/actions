d8407b35a98ba9ace7c2d75dc73ea780ca80588b
podviaznikov <anton@podviaznikov.com>
2019-02-28 17:34:31 -0800

    wip

--- exchange-sync/README.md
+++ exchange-sync/README.md
@@ -29,13 +29,13 @@ It's up to you to define your workflow.
 When the code is pushed `push` flow will be triggered.
 This event should be only used on the master branch.
 New asset will be published automatically with the version in the format:
-`${latest-tag}-NEXT`. *Note*, if repo has new tags then the version will be `0.0.1-NEXT`. 
+`${latest-tag}-NEXT-SNAPSHOT`. *Note*, if repo has new tags then the version will be `0.0.1-NEXT-SNAPSHOT`. 
 
 ### Pull Request
 
 When Pull request is opened or synchronized we will publish new version of the asset
-with the version in the format: `${latest-tag}-PR-${PR_NUMBER}`.
-*Note*, if repo has new tags then the version will be `0.0.1-PR-${PR_NUMBER}`. 
+with the version in the format: `${latest-tag}-PR-${PR_NUMBER}-SNAPSHOT`.
+*Note*, if repo has new tags then the version will be `0.0.1-PR-${PR_NUMBER}-SNAPSHOT`. 
 
 ## Recommended use
 
--- exchange-sync/entrypoint.sh
+++ exchange-sync/entrypoint.sh
@@ -62,7 +62,7 @@ echo "Git latest tags: ${LAST_TAG}"
 ## if push the version is latest tag + '-NEXT'
 if [[ $GITHUB_EVENT_NAME == "push" ]]; then
   echo "handle push"
-  ASSET_VERSION="$LAST_TAG-NEXT"
+  ASSET_VERSION="$LAST_TAG-NEXT-SNAPSHOT"
 fi
 ## if release ASSET version is from the release
 if [[ $GITHUB_EVENT_NAME == "release" ]]; then
@@ -86,11 +86,10 @@ if [[ $GITHUB_EVENT_NAME == "pull_request" ]]; then
       exit 78
     fi
   PR_NUMBER="$(jq -r ".number" "$GITHUB_EVENT_PATH")"
-  ASSET_VERSION="$LAST_TAG-PR-$PR_NUMBER"
-  # TODO check that action is sync or open
+  ASSET_VERSION="$LAST_TAG-PR-$PR_NUMBER-SNAPSHOT"
 fi
 
-echo "Asset version is $ASSET_VERSION"
+echo "New asset version is $ASSET_VERSION"
 
 zip -j -r raml.zip ${RAML_PATH}
 
@@ -98,7 +97,7 @@ echo "Created Zip Archive"
 
 AUTH_HEADER="Authorization: bearer ${ANYPOINT_TOKEN}"
 
-status_code=$(curl -i -X POST \
+status_code=$(curl --verbose -X POST \
    -H "${AUTH_HEADER}" \
    -H "Content-Type:multipart/form-data" \
    -F "name=${ASSET_ID}" \
@@ -112,10 +111,10 @@ status_code=$(curl -i -X POST \
    -F "someFileName=@\"raml.zip\";type=application/zip;filename=\"raml.zip\"" \
  ${ANYPOINT_URL}/exchange/api/v1/assets)
 
-if [[ "$status_code" -ne 201 ]] ; then
-  echo "Errored while pushing to Exchange. Status code: $status_code"
-  exit 3
-fi
+# if [[ "$status_code" -ne 201 ]] ; then
+#   echo "Errored while pushing to Exchange. Status code: $status_code"
+#   exit 3
+# fi
 
 
 exchange_url="${ANYPOINT_URL}/exchange/${ORG_ID}/${ASSET_ID}/"
@@ -130,10 +129,10 @@ echo "$tags_resp"
 echo "Publish tags for assets. Done."
 
 
-if [[ "$status_code" -ne 201 ]] ; then
-  echo "Errored while pushing to Exchange. Status code: $status_code"
-  exit 3
-else
-  echo "==========Finished Anypoint Exchange Sync=========="
-  exit 0
-fi
+# if [[ "$status_code" -ne 201 ]] ; then
+#   echo "Errored while pushing to Exchange. Status code: $status_code"
+#   exit 3
+# else
+#   echo "==========Finished Anypoint Exchange Sync=========="
+#   exit 0
+# fi
